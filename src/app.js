38c69169e795cbd212ac0c0b4ff9a086cc47a8d8
biancardona <biancardona.rivera@gmail.com>
2025-05-19 20:18:58 -0600

    Fixing Cors policy

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+import dotenv from "dotenv";
 const cors = require('cors');
 const path = require('path');
 const { sequelize } = require('./config/database');
@@ -12,14 +13,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+dotenv.config();
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
+app.use(cors({
+    origin: 'frontend-image-gallery.vercel.app',
+    credentials: true
+}));
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/images', imageRoutes);
 app.use('/api/users', userRoutes);
+
+
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
