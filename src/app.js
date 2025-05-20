8db254610eee4aaf9e843138f2d2c950863f0a27
biancardona <biancardona.rivera@gmail.com>
2025-05-20 12:21:35 -0600

    fixing allowed Origins

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS configuration
 const allowedOrigins = [
-    'https://frontend-image-gallery.vercel.app',
+    'https://frontend-image-gallery.vercel.app/api',
     'http://localhost:3000'
 ];
 
